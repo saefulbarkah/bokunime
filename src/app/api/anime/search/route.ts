131e33c47d2ca8cc955e8e3626752943a8363b33
saefulbarkah <saefulbarkah03@gmail.com>
2023-08-30 03:40:04 +0700

    add: search query anime

---
+++ src/app/api/anime/search/route.ts
@@ -0,0 +1,46 @@
+import { scraping } from '@/utils/api';
+import { cheerio } from '@/utils/api/scraping/cheerio';
+import { pagination } from '@/utils/api/scraping/pagination.util';
+import { extractString } from '@/utils/index.util';
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const page = req.nextUrl.searchParams.get('page') || 1;
+  const searchQuery = `page=${page}&${req.nextUrl.searchParams}`;
+  try {
+    const resposne = await scraping.get(`/anime?${searchQuery}`);
+    const html = await resposne.data;
+    const $ = cheerio.load(html);
+
+    const $container = $('.listupd');
+
+    // get lists ongoing
+    const searchLists: any[] = [];
+    $container.find('article').each((idx, el) => {
+      const article = $(el);
+      const div = article.find('div');
+      const slugURL = div.find('a').attr('href') as string;
+      const title = div.find('a .tt').contents().not('h2').text().trim();
+      const type = div.find('a .limit .typez').text().toLowerCase();
+      const status = div.find('a .limit .bt .epx').text().toLowerCase();
+      const thumbnail = div.find('a .limit img').attr('src');
+      searchLists.push({
+        title: title,
+        thumbnail: thumbnail,
+        type: type,
+        status: status,
+        slug: extractString(slugURL, 2, '/'),
+      });
+    });
+
+    const paginate = pagination({ html, page: Number(page) });
+    const data = {
+      ...paginate,
+      collection: searchLists,
+    };
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+}
