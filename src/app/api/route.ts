4ff39603f5062ad58b3bbdb4a93763105930bb9a
saefulbarkah <saefulbarkah03@gmail.com>
2023-09-02 19:57:09 +0700

    fix: some issues

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,5 @@
-import { BASE_URL } from '@/config/siteConfig';
 import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json(`Connected to ${BASE_URL}`);
+  return NextResponse.json(`Welcome to bokunime API`);
 }
